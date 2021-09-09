5a0f7de35ea6977f20b464e27bbd65999b6eaa59
gonzalovazquez <gonzalovazquez010@gmail.com>
2021-09-08 21:59:47 -0400

    feat: 🎸 add Hue lights module

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import {Command, flags} from '@oclif/command'
 import Lifx from './modules/lifx'
 import Weather from './modules/weather'
 import TPLink from './modules/kasa'
+import Hue from './modules/hue'
 
 class Stitch extends Command {
   static description = 'describe the command here'
@@ -17,13 +18,13 @@ class Stitch extends Command {
       char: 'b',
       description: 'toggles Lifx beam',
     }),
-    light: flags.boolean({
-      char: 'l',
-      description: 'toggle Key light',
-    }),
     key: flags.boolean({
       char: 'k',
       description: 'toggle TPLink Key Light',
+    }),
+    desk: flags.boolean({
+      char: 'd',
+      description: 'toggle Hue Monitor Lights',
     }),
   }
 
@@ -33,6 +34,12 @@ class Stitch extends Command {
       required: false,
       description: 'the city',
       default: 'toronto',
+    },
+    {
+      name: 'state',
+      required: false,
+      description: 'the state of the lights',
+      default: true,
     },
   ]
 
@@ -49,6 +56,13 @@ class Stitch extends Command {
 
     if (flags.key) {
       TPLink.toggle()
+    }
+
+    if (flags.desk) {
+      // eslint-disable-next-line no-unneeded-ternary
+      const lightState = args.state === 'true' ? true : false
+      Hue.changeStateLight(14, lightState)
+      Hue.changeStateLight(15, lightState)
     }
   }
 }
--- src/modules/hue.ts
+++ src/modules/hue.ts
@@ -1,7 +1,49 @@
+/* eslint-disable no-console */
 /**
- * Module: Hue Light
+ * Module: Hue
  * Description:
- * Responsable for controlling the Hue Lights
- * Ref: https://developers.meethue.com/develop/hue-api/
+ * Responsable for controlling Hue lights
+ * Ref: https://developers.meethue.com/develop/get-started-2/
  */
 
+const huejay = require('huejay')
+const config = require('../config/config')
+
+const Hue = function () { }
+
+/**
+ * Light Ids:
+ * 13 // Hue Go - Mum
+ * 14 // Hue Play - Right
+ * 15 // Hue Play - Left
+ * 16 // Hue Go - Gonzalo
+ * 17 .. Filament
+ * 18 // Filament
+ * 19 // Lightbulb
+ * 20 // Lightbulm
+ */
+
+// Create client
+const client = new huejay.Client({
+  username: config.hue_username,
+  host: config.hue_ip,
+  port: 80,
+})
+
+Hue.changeStateLight = function (id: number, state: boolean) {
+  return client.lights.getById(id)
+  .then(light => {
+    light.on = state
+    return client.lights.save(light)
+  })
+  .then(light => {
+    const statusName = light.on ? 'on' : 'off'
+    console.log(`Desk Light id: #${light.id} is ${statusName}`)
+  })
+  .catch(error => {
+    console.log('Something went wrong')
+    console.log(error.stack)
+  })
+}
+
+export default Hue
